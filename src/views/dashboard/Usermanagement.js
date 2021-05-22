1ec4984487f63f387c257ecc1f4c0261b7ad12b0
unknown <gendelnamekid@gmail.com>
2021-05-22 21:13:34 +0700

    first commit

---
+++ src/views/dashboard/Usermanagement.js
@@ -0,0 +1,70 @@
+import React, { lazy } from 'react'
+import axios from 'axios'
+
+
+import {
+  CBadge,
+  CCard,
+  CCardBody,
+  CCardHeader,
+  CCol,
+  CDataTable,
+  CRow
+} from '@coreui/react'
+
+
+import { DocsLink } from 'src/reusable'
+
+import usersData from '../users/UsersData'
+
+const getBadge = status => {
+  switch (status) {
+    case 'Active': return 'success'
+    case 'Inactive': return 'secondary'
+    case 'Pending': return 'warning'
+    case 'Banned': return 'danger'
+    default: return 'primary'
+  }
+}
+const fields = ['name','registered', 'role', 'status']
+
+const UserManagement = () => {
+  return (
+    <>
+      <CRow>
+        <CCol>
+          <CCard>
+            <CCardHeader>
+              Combined All dark Table
+            </CCardHeader>
+            <CCardBody>
+            <CDataTable
+              items={usersData}
+              fields={fields}
+              dark
+              hover
+              striped
+              bordered
+              size="sm"
+              itemsPerPage={10}
+              pagination
+              scopedSlots = {{
+                'status':
+                  (item)=>(
+                    <td>
+                      <CBadge color={getBadge(item.status)}>
+                        {item.status}
+                      </CBadge>
+                    </td>
+                  )
+              }}
+            />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
+  )
+}
+
+export default UserManagement
